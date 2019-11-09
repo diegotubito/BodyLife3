f494c13fdf834deb2ecc43ff5d50053140602bad
David Diego Gomez <diegodavid@icloud.com>
2019-11-08 22:09:36 -0300

    Initial Commit

---
+++ BodyLife3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BodyLife3
+//
+//  Created by David Diego Gomez on 08/11/2019.
+//  Copyright © 2019 David Diego Gomez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
